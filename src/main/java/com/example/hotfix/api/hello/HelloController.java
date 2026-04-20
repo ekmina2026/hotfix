ad9114df6fd8e24a13ed0238dbbdbea1f0cc6881
Psaidu <146557741+PsaiduPL@users.noreply.github.com>
2026-04-20 12:34:20 +0200

    add index (#2)

--- src/main/java/com/example/hotfix/api/hello/HelloController.java
+++ src/main/java/com/example/hotfix/api/hello/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.hotfix.api.hello;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
   
   @GetMapping
   public String hello(){
     return "Hello";
+  }
+
+  @GetMapping("/index")
+  public String index(){
+    return "index.html";
   }
 }
