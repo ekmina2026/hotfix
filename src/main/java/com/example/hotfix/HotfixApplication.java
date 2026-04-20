ba2598577b073d08bda790650c5ea2ec76ab8631
PsaiduPL <ekurdziel@student.agh.edu.pl>
2026-04-20 11:42:04 +0200

    init commit

---
+++ src/main/java/com/example/hotfix/HotfixApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotfixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hotfix/HotfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
